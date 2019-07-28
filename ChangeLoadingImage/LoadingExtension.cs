fb48f0e443c60f4191a77aab5cef2fedf2fc3181
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2019-07-28 22:38:16 +0200

    Substract callback

--- ChangeLoadingImage/LoadingExtension.cs
+++ ChangeLoadingImage/LoadingExtension.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Net.Security;
 using System.Reflection;
 using Harmony;
 using ICities;
@@ -9,18 +10,18 @@ namespace ChangeLoadingImage
     public class LoadingExtension : LoadingExtensionBase
     {
         private HarmonyInstance HarmonyInstance;
+        private RemoteCertificateValidationCallback Callback = (sender, cert, chain, sslPolicyErrors) => true;
         
         public override void OnCreated (ILoading loading)
         {
-            ServicePointManager
-                    .ServerCertificateValidationCallback += 
-                (sender, cert, chain, sslPolicyErrors) => true;
+            ServicePointManager.ServerCertificateValidationCallback += Callback;
             HarmonyInstance = HarmonyInstance.Create("github.com/bloodypenguin/ChangeLoadingImage");
             HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly()); 
         }
-
+        
         public override void OnReleased()
         {
+            ServicePointManager.ServerCertificateValidationCallback -= Callback;
             HarmonyInstance?.UnpatchAll();
         }
     }
